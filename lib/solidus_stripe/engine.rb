03322f9a66c291cb28f81f582e6de50827770e0b
Caleb Uriah Harrison <calebuharrison@gmail.com>
2021-01-21 10:05:46 -0600

    simplify engine load

--- lib/solidus_stripe/engine.rb
+++ lib/solidus_stripe/engine.rb
@@ -2,6 +2,8 @@
 
 require 'spree/core'
 
+require 'solidus_stripe'
+
 module SolidusStripe
   class Engine < Rails::Engine
     include SolidusSupport::EngineExtensions
@@ -15,17 +17,17 @@ module SolidusStripe
       g.test_framework :rspec
     end
 
-    if SolidusSupport.backend_available?
-      paths["app/views"] << "lib/views/backend"
-    end
+    # if SolidusSupport.backend_available?
+    #   paths["app/views"] << "lib/views/backend"
+    # end
 
-    if SolidusSupport.frontend_available?
-      paths["app/views"] << "lib/views/frontend"
-    end
+    # if SolidusSupport.frontend_available?
+    #   paths["app/views"] << "lib/views/frontend"
+    # end
 
-    if SolidusSupport.api_available?
-      paths["app/views"] << "lib/views/api"
-    end
+    # if SolidusSupport.api_available?
+    #   paths["app/views"] << "lib/views/api"
+    # end
 
     initializer "spree.payment_method.add_stripe_credit_card", after: "spree.register.payment_methods" do |app|
       app.config.spree.payment_methods << "Spree::PaymentMethod::StripeCreditCard"
